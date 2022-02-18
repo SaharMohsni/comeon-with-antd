0177385a01b51a97d066e6206b316daddd92d539
sahar <sahar.mohsni@gomycode.co>
2022-02-18 16:21:07 +0100

    add header to the gamePage

--- src/pages/gamePage/index.jsx
+++ src/pages/gamePage/index.jsx
@@ -8,30 +8,35 @@ import routes from '../../app/routes/routes';
 import './game-page.scss';
 import { selectLaunchedGame } from '../../features/selectors/games.selectors';
 import { fetchGames } from '../../features/actions/games.actions';
+import GamesPagesHeader from '../../shared/components/gamesPagesHeader';
 const GamePage = () => {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const launchedGame = useSelector(selectLaunchedGame);
-	window.comeon = window.comeon || {};
 
 	useEffect(() => {
 		dispatch(fetchGames());
 	}, []);
 
+	const handleGoBack = () => navigate(routes.GAMES_LIST.path);
+	window.comeon = window.comeon || {};
+
 	useEffect(() => {
 		window.comeon.game.launch(launchedGame.code.toLowerCase());
 	}, []);
 
-	const handleGoBack = () => navigate(routes.GAMES_LIST.path);
-
 	return (
 		<div className="game-page global-page-css ">
+			<div className="game-page__header">
+				<GamesPagesHeader />
+			</div>
+
 			<div className="global-button">
 				<Button icon={<LeftOutlined />} onClick={() => handleGoBack()}>
 					Back to games list
 				</Button>
 			</div>
-			<div id="game-launch" className="game-page__launched-game" />
+			<div id="game-launch" className="game-page__launched-game global-flex-h-center-v-any" />
 			<div className="game-page__details">
 				<div className="game-page__details__name">{launchedGame.name}</div>
 				<p className="game-page__details__description">{launchedGame.description}</p>
